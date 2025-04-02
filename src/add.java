924e8a2a9c5961f9835742ea5abe0602c962fadb
Granth Agarwal <heygranth@gmail.com>
2025-04-02 16:04:17 +0530

    add 2 numbers

---
+++ src/add.java
@@ -0,0 +1,11 @@
+class Add {
+    public static void main(String[] args) {
+        // This is a simple Java program that adds two numbers
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
